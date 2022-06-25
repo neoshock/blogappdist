3e33c35756d3ca9834cfba0b9db989a13cfbe760
neoshock <pideun@gmail.com>
2022-06-24 21:44:39 -0500

    .

--- src/app/services/blog.service.ts
+++ src/app/services/blog.service.ts
@@ -10,13 +10,17 @@ export class BlogService {
       title: 'Blog 1',
       content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
       date: '01/01/2020',
-      tags: ['tag1', 'tag2', 'tag3']
+      tags: ['tag1', 'tag2', 'tag3'],
+      id: 2,
+      autor: 'Autor 2'
     },
     {
       title: 'Blog 2',
       content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
       date: '01/01/2020',
-      tags: ['tag1', 'tag2', 'tag3']
+      tags: ['tag1', 'tag2', 'tag3'],
+      autor: 'Autor 1',
+      id: 2
     },
     {
       title: 'Blog 3',
@@ -24,7 +28,7 @@ export class BlogService {
       date: '01/01/2020',
       tags: ['tag1', 'tag2', 'tag3'],
       autor: 'Autor 1',
-      id: '1'
+      id: 1
     }
   ]
 
@@ -32,5 +36,9 @@ export class BlogService {
 
   getBlogs() {
     return this.blogs;
+  }
+
+  getBlog(id: number) {
+    return this.blogs.find(blog => blog.id === id);
   }
 }
